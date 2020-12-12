4213f894bdcfa42b429f4819d77f3882a75d2acb
marcus30295 <marcus30295@gmail.com>
2020-12-12 16:46:35 +0800

    Error 自訂優化

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -8,19 +8,24 @@ namespace ASPNET5Demo1.Controllers
 {
     public class ErrorController : ControllerBase
     {
-        [HttpGet]
-        [Route("/error")]
-        public ActionResult Error([FromServices]IHostingEnvironment webHostingEnvironment)
+        private readonly IHostEnvironment _webHostingEnvironment;
+        public ErrorController(IHostEnvironment webHostingEnvironment)
+        {
+            this._webHostingEnvironment = webHostingEnvironment;
+        }
+
+        [HttpGet("/error")]
+        public ActionResult Error(/*[FromServices]IHostEnvironment webHostingEnvironment*/)
         {
             var feature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
             var ex = feature?.Error;
-            var isdev = webHostingEnvironment.IsDevelopment();
+            var isDev = _webHostingEnvironment.IsDevelopment();
             var problemDetail = new ProblemDetails
             {
                 Status = (int)HttpStatusCode.InternalServerError,
                 Instance = feature?.Path,
-                Title = isdev?$"{ex.GetType().Name}:{ex.Message}":"An error occured.",
-                Detail = isdev?ex.StackTrace:null,
+                Title = isDev?$"{ex?.GetType().Name}:{ex?.Message}":"An error occured.",
+                Detail = isDev?ex?.StackTrace:null,
             };
 
             return StatusCode(problemDetail.Status.Value,problemDetail);
